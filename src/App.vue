68ad0fb4fceaa4cda6ade80c7e25c80d46b540ed
Kimheng <Kimhengleang45@gmail.com>
2021-04-07 23:01:53 +0700

    Homepage

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+
+// import Header from './components/Header' Global
+export default {
+  name: 'App',
+  components: {
+    // Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Header from './components/Header.vue'
+import 'bootstrap' //npm install bootstrap jquery popper.js 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+const app= createApp(App)
+app.component("Header",Header)
+app.mount('#app')
